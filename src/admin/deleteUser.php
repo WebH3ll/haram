ba82ec24494ee2430a62b7fa4ac44add5ff334dc
1unaram <haram620@icloud.com>
2022-07-21 02:45:16 +0900

    feat: Remark action to delete user for practice

--- src/admin/deleteUser.php
+++ src/admin/deleteUser.php
@@ -1,8 +1,11 @@
 <?
 include "../default_setting.php";
 
-$query = "DELETE FROM user WHERE idx=?";
-$data = $db->query($query, $_GET['idx']);
+// For exploit
+echo "<script>alert('권한 탈취 실습을 위해 실제 삭제 기능은 주석 처리하였습니다.')</script>";
+
+// $query = "DELETE FROM user WHERE idx=?";
+// $data = $db->query($query, $_GET['idx']);
 ?>
 
 <script>
