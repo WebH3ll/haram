6170948888e99614598e9978a738509348389100
1unaram <haram620@icloud.com>
2022-07-20 17:39:42 +0900

    fix: User Agent

--- src/service/pingProc.php
+++ src/service/pingProc.php
@@ -5,12 +5,23 @@ $output;
 $return_var;
 
 if(isset($_GET['ip'])) {
-    exec("ping -c 1 $ip", $output, $return_var);
-    
-    foreach($output as $result) {
-        $ex = iconv("EUC-KR", "UTF-8", $result);
-        echo $ex;
+
+    // Window
+    if($userAgent = preg_match("/Windows/i", $_SERVER["HTTP_USER_AGENT"])) {
+        exec("ping -n 1 $ip", $output, $return_var);
     }
+    // Linux
+    else if($userAgent = preg_match("/Linux/i", $_SERVER["HTTP_USER_AGENT"])) {
+        exec("ping -c 1 $ip", $output, $return_var);
+    }
+
+    $str = '';
+    foreach($output as $result) {
+        $tmp = iconv("EUC-KR", "UTF-8", $result);    // 한글 인코딩
+        $str = $str.$tmp."<br>";
+    }
+
+    echo $str;
 }
 
 ?>
