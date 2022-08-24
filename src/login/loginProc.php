f34b720d35545f4ecd0c5da7595926d0c18c2f1f
Ubuntu <ubuntu@ip-172-31-37-12.ap-northeast-2.compute.internal>
2022-08-24 15:34:03 +0000

    Edit

--- src/login/loginProc.php
+++ src/login/loginProc.php
@@ -1,4 +1,3 @@
-<!-- Change lower -->
 <?
 include "../default_setting.php";
 
@@ -23,30 +22,3 @@ else {
 <script>
     location.href = "/"
 </script>
-
-<!-- 
-Using dbClass
-<?
-include "../default_setting.php";
-
-$uid = $_POST['uid'];
-$upass = $_POST['upass'];
-
-$query = "select * from user where uid=? and upass=md5(?)";
-$data = $db->query($query, $uid, $upass)->fetchArray();
-
-// 로그인 실패
-if (!isset($data['idx'])) {
-    echo "<script>alert('로그인 정보가 잘못 되었습니다.')</script>";
-}
-// 로그인 성공 
-else {
-    $_SESSION['isLogin'] = $uid;
-    setcookie("user_name",  $data['name'], 0, '/');
-}
-?>
-
-<script>
-    location.href = "/"
-</script> 
--->
